6e57751f08629f0d2cc59c43d152ab9529790e3b
Karl Bergström <karl.anton.bergstrom@gmail.com>
2018-09-02 19:21:34 +0200

    README..

--- rust_plugin/src/lib.rs
+++ rust_plugin/src/lib.rs
@@ -5,11 +5,3 @@ pub unsafe extern "C" fn run() {
     script_poc_core::ext_api::simple_test(i, 35);
     }
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
